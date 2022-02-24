565e35df321e8d028ca79117e3e782d94b0d2062
Josh Pollock <jpollock412@gmail.com>
2022-02-24 14:34:13 -0500

    0.8.6

--- jpd2.php
+++ jpd2.php
@@ -3,7 +3,7 @@
 Plugin Name: JPD2
 Plugin URI: http://joshpress.net/blog/jpd2/
 Description: Makes caching WordPress queries via the Transients API easy.
-Version: 0.8.5
+Version: 0.8.6
 Author: Josh Pollock
 Author URI: http://www.JoshPress.net
 License: GPLv2+
